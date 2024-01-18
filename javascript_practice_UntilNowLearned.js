536008abfacabfb3204e4c43fa130c96560f9e91
Prem_Saragada <149216070+PremReddy-585@users.noreply.github.com>
2024-01-18 20:14:05 +0530

    Update javascript_practice_UntilNowLearned.js

--- javascript_practice_UntilNowLearned.js
+++ javascript_practice_UntilNowLearned.js
@@ -11,3 +11,10 @@ function makePersonObject(id, name, email) {
 // Example usage:
 var person = makePersonObject(1, 'John Doe', 'john@example.com');
 console.log(person);
+
+// make an array
+function makeArray(num1, num2, num3){
+    return [num1, num2, num3]
+}
+var CreatedArray = makeArray(1, 2, 3);
+console.log(CreatedArray);
