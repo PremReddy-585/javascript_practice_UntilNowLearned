9a876da45aa283474d8ed8004ca4c1310c60762c
Prem_Saragada <149216070+PremReddy-585@users.noreply.github.com>
2024-01-18 21:58:12 +0530

    Update javascript_practice_UntilNowLearned.js

--- javascript_practice_UntilNowLearned.js
+++ javascript_practice_UntilNowLearned.js
@@ -18,3 +18,16 @@ function makeArray(num1, num2, num3){
 }
 var CreatedArray = makeArray(1, 2, 3);
 console.log(CreatedArray);
+
+
+//creating checkbox uisng DOM 
+let inputElement = document.createElement("input");
+inputElement.type = "checkbox";
+inputElement.id = "myCheckbox";
+
+document.body.appendChild(inputElement);
+
+let labelElement = document.createElement("label");
+labelElement.setAttribute("for", "checkbox" );
+labelElement.textContent = "Graduated";
+document.body.appendChild(labelElement);
